e25558ee8ade6722a32aabcc14b3757f7db350ce
MarcusTL12 <marcuspaafjellet@gmail.com>
2026-06-04 13:58:46 +0200

    Correct using

--- electron_positron.jl
+++ electron_positron.jl
@@ -5,7 +5,7 @@ using GLMakie
 using Printf
 using DSP
 using LinearMaps
-using Arpack
+using IterativeSolvers
 using Preconditioners
 
 # Just to ensure orthonormal basis
@@ -416,7 +416,7 @@ function interactive()
         if !is_solving[]
             is_solving[] = true
 
-            @async begin
+            begin
                 println("Constructing Hamiltonian:")
                 # H = @time construct_hamiltonian(
                 #     max_n[], q1[], q2[], m1[], m2[], d_slider[], L[])
